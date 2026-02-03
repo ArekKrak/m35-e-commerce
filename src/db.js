8f66d3a8bd6810cbbeb9c1d9efc02b94f6849f0a
Arek Krakowiak <369arek12@protonmail.com>
2026-02-03 20:42:41 +0100

    create env and update db file

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const { Pool } = require('pg');
 const pool = new Pool({
-  user: 'arek',
-  host: 'localhost',
-  database: 'ecommerce_api',
-  password: 'password',
-  port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT)
 });
 
 function query(text, params) {
