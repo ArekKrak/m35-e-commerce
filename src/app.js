99cc66449a3765ab48b7d1aa6fda49fb2e0734e9
Arek Krakowiak <369arek12@protonmail.com>
2026-02-04 16:37:08 +0100

    connect app to Postgres

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const db = require('./db.js');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const PORT = 3000;
@@ -10,8 +12,8 @@ app.get('/', (req, res) => {
   res.send('Server running.')
 });
 
-// Prove the route exists and receives data
-app.post('/register', (req, res) => {
+// Prove the route exists and receives data using /register handler
+app.post('/register', async (req, res) => {
   // Validate input
   // 1. Extract fields
   const { email, password } = req.body || {};
@@ -21,9 +23,30 @@ app.post('/register', (req, res) => {
     return res.status(400).json({ error: 'email and password are required' });
   }
   // 3. Otherwise, return 200
-  return res.status(200).json({ email });
+  const passwordHash = await bcrypt.hash(password, 10);
+  // try block to throw a clean API response should the same email be registered
+  // twice.
+  try {
+    const result = await db.query(
+      // SQL query, $1, $2 are placeholders to prevent SQL injections
+      'INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email',
+      [email, passwordHash]
+    );
+    // status 201 (created)
+    return res.status(201).json(result.rows[0]);
+  } catch (err) {
+    // `23505` is Postgres's error for unique constrant violation
+    if (err.code === '23505') {
+      // 409 - Conflict error
+      return res.status(409).json({ error: 'user already exists' });
+    }
+    console.error(err);
+    // 500 - Internal Server Error
+    return res.status(500).json({ error: 'internal server error' });
+  }
 });
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
