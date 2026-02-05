b3af040f0bf69b636ac2e4902f5c886834408544
Arek Krakowiak <369arek12@protonmail.com>
2026-02-05 20:05:44 +0100

    add products id

--- src/products.routes.js
+++ src/products.routes.js
@@ -11,11 +11,23 @@ router.get('/', async (req, res) => {
     if (!Number.isInteger(categoryId)) {
       return res.status(400).json({ error: 'Category must be an integer id' });
     }
-    const filteredResult = await db.query('SELECT * FROM products WHERE category_id = $1 ORDER BY id', [categoryId]);
+    const filteredResult = await db.query('SELECT id, name, price, category_id FROM products WHERE category_id = $1 ORDER BY id', [categoryId]);
     return res.json(filteredResult.rows);
+  }
+  const result = await db.query('SELECT * FROM products ORDER BY id');
+  return res.json(result.rows);
+});
+
+router.get('/:productId', async (req, res) => {
+  const id = Number(req.params.productId);
+  if (!Number.isInteger(id)) {
+    return res.status(400).json({ error: 'Product id must be an integer' });
+  }
+  const idResult = await db.query ('SELECT * FROM products WHERE id = $1', [id]);
+  if (idResult.rows.length === 0) {
+    return res.status(404).json({ error: 'Product not found' });
   } else {
-    const result = await db.query('SELECT * FROM products ORDER BY id');
-    return res.json(result.rows);
+    return res.json(idResult.rows[0]);
   }
 });
 
